652a1112398017008a3a14d11b57acd8d7904fb6
Angad Singh <angadsingh567890@gmail.com>
2019-03-05 16:41:46 +0530

    Update react app

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      loading: true,
-      response: ''
-    }
-  }
-
-  componentDidMount() {
-    fetch('/api/auth')
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          loading: false,
-          response: res.message
-        })
-      })
-      .catch(e => console.error(e))
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            {this.state.loading ? 'Loading...' : this.state.response}
+            Edit <code>src/App.js</code> and save to reload.
           </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
