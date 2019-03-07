1916cfe55c1556d6be1d0fcd272e80f5a40f52a7
Angad Singh <angadsingh567890@gmail.com>
2019-03-07 08:40:23 +0530

    Mounted routes take precedence over react routes

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const apiRoutes = require('./api')
 const fs = require('fs')
 const rtr = new Router()
 
+// Mount API routes
+rtr.use('/api', apiRoutes.routes(), apiRoutes.allowedMethods())
+
 // Serve react app
-rtr.get(/^\/[^/]*$/, async ctx => {
+rtr.get(/(^\/[^/])*$/, async ctx => {
   ctx.set('Content-Type', 'text/html')
   ctx.body = fs.createReadStream('./static/index.html')
 })
-
-// Mount API routes
-rtr.use('/api', apiRoutes.routes(), apiRoutes.allowedMethods())
 
 // Handle 404s
 rtr.all('*', async ctx => ctx.throw(ctx, 404, 'Resource not found'))
