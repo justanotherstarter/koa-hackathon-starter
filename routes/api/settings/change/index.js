abfad47e19f456e1d686052dbfacc7d0a3229bd5
Angad Singh <angadsingh567890@gmail.com>
2019-03-15 21:59:59 +0530

    Use appropriate request methods

--- routes/api/settings/change/index.js
+++ routes/api/settings/change/index.js
@@ -5,15 +5,15 @@ const emailHandler = require('./email')
 const passwordHandler = require('./password')
 const rtr = new Router()
 
-rtr.post(
+rtr.put(
   '/username',
   validateAgainst(usernameHandler.schema),
   usernameHandler.handler
 )
 
-rtr.post('/email', validateAgainst(emailHandler.schema), emailHandler.handler)
+rtr.put('/email', validateAgainst(emailHandler.schema), emailHandler.handler)
 
-rtr.post(
+rtr.put(
   '/password',
   validateAgainst(passwordHandler.schema),
   passwordHandler.handler
